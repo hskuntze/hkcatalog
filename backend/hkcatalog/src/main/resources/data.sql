a03fac5f89c86d7a82e1a3e37a4693d430634b19
Hassan <hskuntze@gmail.com>
2021-11-21 16:37:09 -0300

    Database Seeding & Transaction

---
+++ backend/hkcatalog/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO tb_category (name) VALUES ('Books');
+INSERT INTO tb_category (name) VALUES ('Electronics');
+INSERT INTO tb_category (name) VALUES ('Computers');
+INSERT INTO tb_category (name) VALUES ('Movies');
+INSERT INTO tb_category (name) VALUES ('Beverages');
+INSERT INTO tb_category (name) VALUES ('Food');
