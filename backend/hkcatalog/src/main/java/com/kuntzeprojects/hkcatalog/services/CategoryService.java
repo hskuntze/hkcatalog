de0efbb6b371d63b7bb4b98765180db11d59e5d6
Hassan <hskuntze@gmail.com>
2021-11-23 21:11:13 -0300

    Category by id; Tratamento de exceções

--- backend/hkcatalog/src/main/java/com/kuntzeprojects/hkcatalog/services/CategoryService.java
+++ backend/hkcatalog/src/main/java/com/kuntzeprojects/hkcatalog/services/CategoryService.java
@@ -1,6 +1,7 @@
 package com.kuntzeprojects.hkcatalog.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.kuntzeprojects.hkcatalog.dto.CategoryDTO;
 import com.kuntzeprojects.hkcatalog.entities.Category;
 import com.kuntzeprojects.hkcatalog.repositories.CategoryRepository;
+import com.kuntzeprojects.hkcatalog.services.exceptions.EntityNotFoundException;
 
 @Service
 public class CategoryService {
@@ -23,4 +25,12 @@ public class CategoryService {
 		
 		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 	}
+	
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entidade não foi encontrada."));
+		return new CategoryDTO(entity);
+	}
+	
 }
---
+++ backend/hkcatalog/src/main/java/com/kuntzeprojects/hkcatalog/services/exceptions/EntityNotFoundException.java
@@ -0,0 +1,15 @@
+package com.kuntzeprojects.hkcatalog.services.exceptions;
+
+
+/**
+ * Extender uma classe de exceção com "Exception" obriga o aplicativo a tratar a exceção.
+ * Para uma aplicação mais flexível, é recomendável utilizar o "RuntimeException" pois podemos tratá-la, ou não.
+ * @author Kuntze
+ */
+public class EntityNotFoundException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+	
+	public EntityNotFoundException(String msg) {
+		super(msg);
+	}
+}
