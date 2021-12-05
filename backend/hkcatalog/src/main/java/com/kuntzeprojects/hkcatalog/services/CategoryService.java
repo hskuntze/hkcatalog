12fec60aefefaf8677b45ce210f3db7513952442
Hassan <hskuntze@gmail.com>
2021-12-05 11:07:21 -0300

    Dados de auditoria; Entidade Product

--- backend/hkcatalog/src/main/java/com/kuntzeprojects/hkcatalog/services/CategoryService.java
+++ backend/hkcatalog/src/main/java/com/kuntzeprojects/hkcatalog/services/CategoryService.java
@@ -7,6 +7,8 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -27,6 +29,12 @@ public class CategoryService {
 		List<Category> list = repository.findAll();
 		
 		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+	}
+	
+	@Transactional(readOnly = true)
+	public Page<CategoryDTO> findAllPaged(PageRequest pageRequest){
+		Page<Category> list = repository.findAll(pageRequest);
+		return list.map(x -> new CategoryDTO(x));
 	}
 	
 	@Transactional(readOnly = true)
