049de279fffc283c37e21c399ab664a788a4ef0d
Hassan <hskuntze@gmail.com>
2021-12-27 18:46:43 -0300

    ProductService tests and behaviors with Mockito

--- backend/hkcatalog/src/test/java/com/kuntzeprojects/hkcatalog/services/ProductServiceTests.java
+++ backend/hkcatalog/src/test/java/com/kuntzeprojects/hkcatalog/services/ProductServiceTests.java
@@ -1,16 +1,34 @@
 package com.kuntzeprojects.hkcatalog.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import javax.persistence.EntityNotFoundException;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import com.kuntzeprojects.hkcatalog.dto.ProductDTO;
+import com.kuntzeprojects.hkcatalog.entities.Category;
+import com.kuntzeprojects.hkcatalog.entities.Product;
+import com.kuntzeprojects.hkcatalog.repositories.CategoryRepository;
 import com.kuntzeprojects.hkcatalog.repositories.ProductRepository;
+import com.kuntzeprojects.hkcatalog.services.exceptions.DatabaseException;
+import com.kuntzeprojects.hkcatalog.services.exceptions.ResourceNotFoundException;
+import com.kuntzeprojects.hkcatalog.tests.Factory;
 
 @ExtendWith(SpringExtension.class)
 public class ProductServiceTests {
@@ -20,16 +38,39 @@ public class ProductServiceTests {
 	@Mock
 	private ProductRepository repository;
 	
+	@Mock
+	private CategoryRepository catRepository;
+	
 	private Long existingId;
 	private Long nonExistingId;
+	private Long dependentId;
+	private PageImpl<Product> page;
+	private Product product;
+	private Category category;
 	
 	@BeforeEach
 	void setUp() throws Exception{
 		existingId = 1L;
 		nonExistingId = 1000L;
+		dependentId = 4L;
+		product = Factory.createProduct();
+		category = Factory.createCategory();
+		page = new PageImpl<>(List.of(product));
 		
 		Mockito.doNothing().when(repository).deleteById(existingId);
 		Mockito.doThrow(EmptyResultDataAccessException.class).when(repository).deleteById(nonExistingId);
+		Mockito.doThrow(DataIntegrityViolationException.class).when(repository).deleteById(dependentId);
+		
+		Mockito.when(repository.findAll((Pageable) ArgumentMatchers.any())).thenReturn(page);
+		Mockito.when(repository.findById(existingId)).thenReturn(Optional.of(product));
+		Mockito.when(repository.findById(nonExistingId)).thenReturn(Optional.empty());
+		Mockito.when(repository.save(ArgumentMatchers.any())).thenReturn(product);
+		
+		Mockito.when(repository.getOne(existingId)).thenReturn(product);
+		Mockito.when(repository.getOne(nonExistingId)).thenThrow(EntityNotFoundException.class);
+		
+		Mockito.when(catRepository.getOne(existingId)).thenReturn(category);
+		Mockito.when(catRepository.getOne(nonExistingId)).thenThrow(EntityNotFoundException.class);
 	}
 	
 	@Test
@@ -39,5 +80,63 @@ public class ProductServiceTests {
 		});
 		
 		Mockito.verify(repository, Mockito.times(1)).deleteById(existingId);
+	}
+	
+	@Test
+	public void deleteShouldThrowResourceNotFoundExceptionWhenIdDoesNotExist() {
+		Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+			service.delete(nonExistingId);
+		});
+		
+		Mockito.verify(repository, Mockito.times(1)).deleteById(nonExistingId);
+	}
+	
+	@Test
+	public void deleteShouldThrowDatabaseExceptionWhenIdHasDependent() {
+		Assertions.assertThrows(DatabaseException.class, () -> {
+			service.delete(dependentId);
+		});
+		
+		Mockito.verify(repository, Mockito.times(1)).deleteById(dependentId);
+	}
+	
+	@Test
+	public void findAllPagedShouldReturnPage() {
+		Pageable pageable = PageRequest.of(0, 10);
+		Page<ProductDTO> result = service.findAllPaged(pageable);
+		
+		Assertions.assertNotNull(result);
+		Mockito.verify(repository, Mockito.times(1)).findAll(pageable);
+	}
+	
+	@Test
+	public void findByIdShouldReturnProductDTOWhenIdExists() {
+		ProductDTO obj = service.findById(existingId);
+		
+		Assertions.assertNotNull(obj);
+		Mockito.verify(repository, Mockito.times(1)).findById(existingId);
+	}
+	
+	@Test
+	public void findByIdShouldThrowResourceNotFoundExceptionWhenIdDoesNotExist() {
+		Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+			service.findById(nonExistingId);
+		});
+	}
+	
+	@Test
+	public void updateShouldReturnProductDTOWhenIdExists() {
+		ProductDTO productDto = Factory.createProductDTO();
+		ProductDTO result = service.update(existingId, productDto);
+		
+		Assertions.assertNotNull(result);
+	}
+	
+	@Test
+	public void updateShouldThrowResourceNotFoundExceptionWhenIdDoesNotExist() {
+		ProductDTO productDto = Factory.createProductDTO();
+		Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+			service.update(nonExistingId, productDto);
+		});
 	}
 }
--- backend/hkcatalog/src/test/java/com/kuntzeprojects/hkcatalog/tests/Factory.java
+++ backend/hkcatalog/src/test/java/com/kuntzeprojects/hkcatalog/tests/Factory.java
@@ -10,12 +10,16 @@ public class Factory {
 	
 	public static Product createProduct() {
 		Product product = new Product(1L, "iPhone 13", 14599.99, "New iPhone", "https://img.com/img.png", Instant.parse("2021-12-21T01:12:41Z")); 
-		product.getCategories().add(new Category(2L, "Electronics"));
+		product.getCategories().add(createCategory());
 		return product;
 	}
 	
 	public static ProductDTO createProductDTO() {
 		Product p = createProduct();
 		return new ProductDTO(p, p.getCategories());
+	}
+	
+	public static Category createCategory() {
+		return new Category(1L, "Category X");
 	}
 }
