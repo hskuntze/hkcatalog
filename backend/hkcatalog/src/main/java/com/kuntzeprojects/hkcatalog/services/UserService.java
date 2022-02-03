021e0cbfbf831869637ea9ab45042241e4d295bd
Hassan <hskuntze@gmail.com>
2022-02-03 10:33:25 -0300

    Spring Security Checklist & OAuth2 Authorization Server checklist

--- backend/hkcatalog/src/main/java/com/kuntzeprojects/hkcatalog/services/UserService.java
+++ backend/hkcatalog/src/main/java/com/kuntzeprojects/hkcatalog/services/UserService.java
@@ -6,11 +6,16 @@ import java.util.stream.Collectors;
 
 import javax.persistence.EntityNotFoundException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -27,7 +32,9 @@ import com.kuntzeprojects.hkcatalog.services.exceptions.DatabaseException;
 import com.kuntzeprojects.hkcatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
+	
+	private static Logger logger = LoggerFactory.getLogger(UserService.class);
 	
 	@Autowired
 	private BCryptPasswordEncoder encoder;
@@ -99,5 +106,16 @@ public class UserService {
 			Role role = roleRepository.getOne(roleDto.getId());
 			entity.getRoles().add(role);
 		}
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = repository.findByEmail(username);
+		if(user == null) {
+			logger.error("Usuário não foi encontrado " + username);
+			throw new UsernameNotFoundException("Usuário não foi encontrado.");
+		}
+		logger.info(username + " foi encontrado.");
+		return user;
 	}
 }
