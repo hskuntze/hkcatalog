5efe7a05c171a00055904bca758bb5501476971c
Hassan <hskuntze@gmail.com>
2022-04-27 15:24:29 -0300

    Props

--- frontend/src/components/ButtonIcon/index.tsx
+++ frontend/src/components/ButtonIcon/index.tsx
@@ -1,12 +1,16 @@
 import './styles.css';
 import { ReactComponent as ArrowIcon } from 'assets/images/Seta.svg';
 
-function ButtonIcon() {
+type Props = {
+  text: string;
+}
+
+function ButtonIcon({text} : Props) {
   return (
     <>
       <div className="btn-container">
         <button className="btn btn-primary">
-          <h6 className="text-uppercase">Inicie agora a sua busca</h6>
+          <h6 className="text-uppercase">{text}</h6>
         </button>
         <div className="btn-icon">
           <ArrowIcon />
--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -10,25 +10,48 @@ function Navbar() {
           <Link to="/" className="nav-logo-text">
             <h4>HK Catalog</h4>
           </Link>
-          <button className='navbar-toggler'
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#catalog-navbar"
-          aria-controls="catalog-navbar"
-          aria-expanded="false"
-          aria-label="Toggle navigation">
-              <span className='navbar-toggler-icon'></span>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#catalog-navbar"
+            aria-controls="catalog-navbar"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div className='collapse navbar-collapse' id="catalog-navbar">
-            <ul className='navbar-nav offset-md-2 main-menu'>
+          <div className="collapse navbar-collapse" id="catalog-navbar">
+            <ul className="navbar-nav offset-md-2 main-menu">
               <li>
-                <NavLink to="/" style={({ isActive }) => ({ color: isActive ? 'white' : '#e5e5e5' })}>HOME</NavLink>
+                <NavLink
+                  to="/"
+                  style={({ isActive }) => ({
+                    color: isActive ? 'white' : '#e5e5e5',
+                  })}
+                >
+                  HOME
+                </NavLink>
               </li>
               <li>
-                <NavLink to="/products" style={({ isActive }) => ({ color: isActive ? 'white' : '#e5e5e5' })}>CATALOGO</NavLink>
+                <NavLink
+                  to="/products"
+                  style={({ isActive }) => ({
+                    color: isActive ? 'white' : '#e5e5e5',
+                  })}
+                >
+                  CATALOGO
+                </NavLink>
               </li>
               <li>
-                <NavLink to="/admin" style={({ isActive }) => ({ color: isActive ? 'white' : '#e5e5e5' })}>ADMIN</NavLink>
+                <NavLink
+                  to="/admin"
+                  style={({ isActive }) => ({
+                    color: isActive ? 'white' : '#e5e5e5',
+                  })}
+                >
+                  ADMIN
+                </NavLink>
               </li>
             </ul>
           </div>
