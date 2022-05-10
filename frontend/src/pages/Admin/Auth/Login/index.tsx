ac5728ca397860bc45131bde06b02f5b42c93ac2
Hassan <hskuntze@gmail.com>
2022-05-10 12:40:37 -0300

    Validação com React Hook Form

--- frontend/src/pages/Admin/Auth/Login/index.tsx
+++ frontend/src/pages/Admin/Auth/Login/index.tsx
@@ -15,7 +15,7 @@ const Login = () => {
 
   const [hasError, setHasError] = useState(false);
 
-  const { register, handleSubmit } = useForm<FormData>();
+  const { register, handleSubmit, formState: {errors} } = useForm<FormData>();
 
   const onSubmit = (formData : FormData) => {
     requestBackendLogin(formData)
@@ -39,16 +39,26 @@ const Login = () => {
               type="text"
               className="form-control base-input"
               placeholder="Email"
-              {...register("username")}
+              {...register("username", {
+                required: 'Campo obrigatório',
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                  message: 'Email inválido'
+                }
+              })}
             />
+            <div className='invalid-feedback d-block'>{errors.username?.message}</div>
           </div>
           <div className="mb-2">
             <input
-              {...register("password")}
+              {...register("password", {
+                required: 'Campo obrigatório'
+              })}
               type="password"
               className="form-control base-input"
               placeholder="Password"
             />
+            <div className='invalid-feedback d-block'>{errors.password?.message}</div>
           </div>
           {hasError && (
             <div className="alert alert-danger">Credenciais inválidas</div>
