ec2069daeeaee6feb296cfdb4deb764fe3c02b4e
Hassan <hskuntze@gmail.com>
2022-05-16 12:40:08 -0300

    Função hasAnyRoles no Frontend

--- frontend/src/util/requests.ts
+++ frontend/src/util/requests.ts
@@ -109,3 +109,30 @@ export const isAuthenticated = (): boolean => {
   const tokenData = getTokenData();
   return (tokenData && tokenData.exp * 1000 > Date.now()) ? true : false;
 };
+
+export const hasAnyRoles = (roles : Role[]): boolean => {
+  if(roles.length === 0){
+    return true;
+  }
+
+  const tokenData = getTokenData();
+  
+
+  //Testando se há role com High Order Function
+  if(tokenData !== undefined){
+    return roles.some(role => tokenData.authorities.includes(role));
+  }
+
+  //Testando se há role com lógica padrão
+  /*
+  if(tokenData !== undefined){
+    for(var i = 0; i < roles.length; i++){
+      if(tokenData.authorities.includes(roles[i])){
+        return true;
+      }
+    }
+  }
+  */
+
+  return false;
+}
