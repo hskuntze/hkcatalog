c94e76db9cb1ce8a0c917e3035ff63f33a09ddc3
Hassan <hskuntze@gmail.com>
2022-05-19 19:00:58 -0300

    Pagina de Formulario - HTML e CSS Base; DB Seed fix no backend

--- frontend/src/pages/Admin/Products/Form/index.tsx
+++ frontend/src/pages/Admin/Products/Form/index.tsx
@@ -1,7 +1,44 @@
+import './styles.css';
+
 const Form = () => {
-    return (
-        <h1>formulário</h1>
-    )
-}
+  return (
+    <div className="product-crud-container">
+      <div className="base-card form-crud-card">
+        <h1>DADOS DO PRODUTO</h1>
+        <form>
+          <div className="row form-crud-input">
+            <div className="col-lg-6 inputs-left-container">
+              <div className="margin-bottom-30">
+                <input type="text" className="form-control base-input" />
+              </div>
+              <div className="margin-bottom-30">
+                <input type="text" className="form-control base-input" />
+              </div>
+              <div>
+                <input type="text" className="form-control base-input" />
+              </div>
+            </div>
+            <div className="col-lg-6">
+              <div>
+                <textarea
+                  rows={5}
+                  className="form-control base-input h-auto"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="form-crud-button-container">
+            <button className="btn btn-outline-danger form-crud-button">
+              CANCELAR
+            </button>
+            <button className="btn btn-primary form-crud-button text-white">
+              SALVAR
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default Form;
--- frontend/src/pages/Admin/Products/List/index.tsx
+++ frontend/src/pages/Admin/Products/List/index.tsx
@@ -1,35 +1,31 @@
+import { AxiosRequestConfig } from 'axios';
 import ProductCrudCard from 'pages/Admin/Products/ProductCrudCard';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { Product } from 'types/product';
+import { SpringPage } from 'types/vendor/spring';
+import { requestBackend } from 'util/requests';
 import './styles.css';
 
 const List = () => {
-  const product = {
-    id: 2,
-    name: 'Smart TV',
-    price: 2190.0,
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    imgUrl:
-      'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg',
-    date: '2020-07-14T10:00:00Z',
-    categories: [
-      {
-        id: 1,
-        name: 'Books',
+  const [page, setPage] = useState<SpringPage<Product>>();
+  useEffect(() => {
+    const params: AxiosRequestConfig = {
+      method: 'GET',
+      url: '/products',
+      params: {
+        page: 0,
+        size: 50,
       },
-      {
-        id: 3,
-        name: 'Computers',
-      },
-      {
-        id: 4,
-        name: 'Cellphones',
-      },
-    ],
-  };
+    };
+
+    requestBackend(params).then((response) => {
+      setPage(response.data);
+    });
+  }, []);
 
   return (
-    <>
+    <div className="product-crud-container">
       <div className="product-crud">
         <Link to="/admin/products/create">
           <button className="btn btn-primary text-white btn-add">
@@ -39,20 +35,13 @@ const List = () => {
         <div className="base-card search-bar">Search Bar</div>
       </div>
       <div className="row">
-        <div className="col-sm-6 col-md-12">
-          <ProductCrudCard product={product} />
-        </div>
-        <div className="col-sm-6 col-md-12">
-          <ProductCrudCard product={product} />
-        </div>
-        <div className="col-sm-6 col-md-12">
-          <ProductCrudCard product={product} />
-        </div>
-        <div className="col-sm-6 col-md-12">
-          <ProductCrudCard product={product} />
-        </div>
+        {page?.content.map((product) => (
+          <div className="col-sm-6 col-md-12" key={product.id}>
+            <ProductCrudCard product={product} />
+          </div>
+        ))}
       </div>
-    </>
+    </div>
   );
 };
 
